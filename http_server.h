cc65f0ce3bb06f9c5ce1b14ff775a436b315a927
Paintakotako <Paintako@p76111652.gs.ncku.edu.tw>
2023-06-03 23:00:40 +0800

    Feat: Adding new structure
    Adding a new struct, there's two members in it, a boolean to tell
    if the workqueue shold be destoyed or not, and a `list_head`
    to store all the works.

--- http_server.h
+++ http_server.h
@@ -1,10 +1,19 @@
 #ifndef KHTTPD_HTTP_SERVER_H
 #define KHTTPD_HTTP_SERVER_H
 
+#include <linux/module.h>
+#include <linux/workqueue.h>
 #include <net/sock.h>
+#define MODULE_NAME "khttpd"
+
 
 struct http_server_param {
     struct socket *listen_socket;
+};
+
+struct khttpd_service {
+    bool is_stopped;
+    struct list_head worker;
 };
 
 extern int http_server_daemon(void *arg);
