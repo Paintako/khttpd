92e1a5cf4657a00033c7926a4cea9d802e90c8a4
Paintakotako <Paintako@p76111652.gs.ncku.edu.tw>
2023-06-06 15:08:15 +0800

    Feat: directory listing function added
    New function `handle_directory` added, this function is for
    listing the given directory, but no navigate function yet.

--- http_server.c
+++ http_server.c
@@ -31,6 +31,7 @@
     "Connection: KeepAlive" CRLF CRLF "501 Not Implemented" CRLF
 
 #define RECV_BUFFER_SIZE 4096
+#define SEND_BUFFER_SIZE 256
 
 struct khttpd_service daemon = {.is_stopped = false};
 extern struct workqueue_struct *khttpd_wq;
@@ -42,6 +43,7 @@ struct http_request {
     int complete;
     struct list_head list;
     struct work_struct khttpd_work;
+    struct dir_context dir_context;  // struct dir_context, defines in fs.h
 };
 
 static int http_server_recv(struct socket *sock, char *buf, size_t size)
@@ -78,17 +80,81 @@ static int http_server_send(struct socket *sock, const char *buf, size_t size)
     return done;
 }
 
+static int tracedir(struct dir_context *dir_context,
+                    const char *name,
+                    int namelen,
+                    loff_t offset,
+                    u64 ino,
+                    unsigned int d_type)
+{
+    if (strcmp(name, ".") && strcmp(name, "..")) {
+        struct http_request *request =
+            container_of(dir_context, struct http_request, dir_context);
+        char buf[SEND_BUFFER_SIZE] = {0};
+
+        snprintf(buf, SEND_BUFFER_SIZE,
+                 "<tr><td><a href=\"%s\">%s</a></td></tr>\r\n", name, name);
+        http_server_send(request->socket, buf, strlen(buf));
+    }
+    return 0;
+}
+
+static bool handle_directory(struct http_request *request)
+{
+    struct file *fp;
+    char buf[SEND_BUFFER_SIZE] = {0};
+
+    request->dir_context.actor = tracedir;
+    if (request->method != HTTP_GET) {
+        snprintf(buf, SEND_BUFFER_SIZE,
+                 "HTTP/1.1 501 Not Implemented\r\n%s%s%s%s",
+                 "Content-Type: text/plain\r\n", "Content-Length: 19\r\n",
+                 "Connection: Close\r\n", "501 Not Implemented\r\n");
+        http_server_send(request->socket, buf, strlen(buf));
+        return false;
+    }
+
+    snprintf(buf, SEND_BUFFER_SIZE, "HTTP/1.1 200 OK\r\n%s%s%s",
+             "Connection: Keep-Alive\r\n", "Content-Type: text/html\r\n",
+             "Keep-Alive: timeout=5, max=1000\r\n\r\n");
+    http_server_send(request->socket, buf, strlen(buf));
+
+
+    snprintf(buf, SEND_BUFFER_SIZE, "%s%s%s%s", "<html><head><style>\r\n",
+             "body{font-family: monospace; font-size: 15px;}\r\n",
+             "td {padding: 1.5px 6px;}\r\n",
+             "</style></head><body><table>\r\n");
+    http_server_send(request->socket, buf, strlen(buf));
+
+    fp = filp_open("/home/paintako/", O_RDONLY | O_DIRECTORY, 0);
+    if (IS_ERR(fp)) {
+        pr_info("Open file failed");
+        return false;
+    }
+
+    iterate_dir(fp, &request->dir_context);
+    snprintf(buf, SEND_BUFFER_SIZE, "</table></body></html>\r\n");
+    http_server_send(request->socket, buf, strlen(buf));
+    filp_close(fp, NULL);
+    return true;
+}
+
 static int http_server_response(struct http_request *request, int keep_alive)
 {
-    char *response;
+    // char *response;
 
-    pr_info("requested_url = %s\n", request->request_url);
-    if (request->method != HTTP_GET)
-        response = keep_alive ? HTTP_RESPONSE_501_KEEPALIVE : HTTP_RESPONSE_501;
-    else
-        response = keep_alive ? HTTP_RESPONSE_200_KEEPALIVE_DUMMY
-                              : HTTP_RESPONSE_200_DUMMY;
-    http_server_send(request->socket, response, strlen(response));
+    // pr_info("requested_url = %s\n", request->request_url);
+    // if (request->method != HTTP_GET)
+    //     response = keep_alive ? HTTP_RESPONSE_501_KEEPALIVE :
+    //     HTTP_RESPONSE_501;
+    // else
+    //     response = keep_alive ? HTTP_RESPONSE_200_KEEPALIVE_DUMMY
+    //                           : HTTP_RESPONSE_200_DUMMY;
+    // http_server_send(request->socket, response, strlen(response));
+
+    if (handle_directory(request) == 0)
+        printk("Something went wrong\n");
+
     return 0;
 }
 
